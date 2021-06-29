9e7266a366bb423b6be22f711ac7b047f7c76891
GreenSnak <86664888+GreenSnak@users.noreply.github.com>
2021-06-29 16:35:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.morev').on('click', function (){
+  $('.moreb-vacancies').addClass('dnone');
+  $('.block').addClass('dblock');
+});
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.navbar');
+const closeMenuButton = document.querySelector('.close-button');
+const toggleMenu = (event) => {
+  menu.classList.toggle('is-open');
+};
+
+menuButton.addEventListener('click', toggleMenu);
+closeMenuButton.addEventListener('click', toggleMenu);
